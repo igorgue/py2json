883df8ce28b92fcf53da1c516a9a0995292310f3
Igor Guerrero Fonseca <igfgt1@gmail.com>
2009-07-16 22:51:54 -0400

    now generated by paster

--- setup.py
+++ setup.py
@@ -1,46 +1,26 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
+import sys, os
 
- # 
- # Copyright 2009 
- #
- # Licensed under the Apache License, Version 2.0 (the "License");
- # you may not use this file except in compliance with the License.
- # You may obtain a copy of the License at
- #
- #      http://www.apache.org/licenses/LICENSE-2.0
- #
- # Unless required by applicable law or agreed to in writing, software
- # distributed under the License is distributed on an "AS IS" BASIS,
- # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- # See the License for the specific language governing permissions and
- # limitations under the License.
+version = '0.1'
 
-from setuptools import setup
-import os
-
-#install_requires = [
-#]
-
-version = '0.0.0'
-
-basedir = os.path.dirname(__file__)
-README = os.path.join(basedir, 'README')
-long_description = open(README).read() + 'nn'
-
-#scriptdir = os.path.join(basedir, 'bin')
-#exes = os.listdir(scriptdir)
-#scripts = filter(os.path.isfile,[os.path.join(scriptdir, x) for x in exes])
-
-setup(
-	name="Py2JSON",
-	description="Python to JSON translator",
-	url='http://www.igorgue.info/',
-	author="Igor Guerrero",
-	license='ASLv2',
-	version=version,
-	pymodules=['py2json'],
-	#install_requires=install_requires,
-	#test_suite='test',
-	long_description=long_description,
-	#scripts=scripts,
-	)
+setup(name='py2json',
+      version=version,
+      description="Python to JSON-schema translator",
+      long_description="""\
+== ABOUT ==""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='json json-schema translator',
+      author='Igor Guerrero',
+      author_email='igfgt1@gmail.com',
+      url='http://app.arat.us/',
+      license='Apache License Version 2.0',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
